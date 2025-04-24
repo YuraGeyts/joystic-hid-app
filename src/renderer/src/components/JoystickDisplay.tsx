58c27ee03840d63d30d0e5036af6313292296401
YuraGeyts <yura.geyts@gmail.com>
2025-04-24 10:08:57 +0300

    refactor, create PWMTable component

--- src/renderer/src/components/JoystickDisplay.tsx
+++ src/renderer/src/components/JoystickDisplay.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { JoystickOutputInterface } from '../../../models/JoystickOutput'
 import JoystickSquare from './JoystickSquare'
+import PWMTable from './PWMTable';
 
 const JoystickDisplay: React.FC = () => {
   // State to store joystick data
@@ -26,47 +27,7 @@ const JoystickDisplay: React.FC = () => {
 
   // Progress calculation function
   const calculateProgress = (pwmValue: number) => {
-    return ((pwmValue - 1000) / (2000 - 1000)) * 100;
-  };
-
-  const renderPWMTable = () => {
-    if (!joystickData) return null;
-
-    const excludedChannels = ['yaw', 'throttle', 'roll', 'pitch']
-    const filteredData = Object.entries(joystickData).filter(([channel]) => !excludedChannels.includes(channel))
-
-    return (
-      <div style={styles.centerContainer}>
-        <table>
-          <thead>
-            <tr>
-              <th>Type</th>
-              <th>PWM</th>
-              <th>Progress</th>
-            </tr>
-          </thead>
-          <tbody>
-            {filteredData.map(([channel, pwmValue]) => (
-              <tr key={channel}>
-                <td>{channel}</td>
-                <td>{pwmValue}</td>
-                <td>
-                  <div style={{ width: '100%', backgroundColor: '#ddd', height: '12px' }}>
-                    <div
-                      style={{
-                        width: `${calculateProgress(pwmValue)}%`,
-                        backgroundColor: 'green',
-                        height: '100%',
-                      }}
-                    />
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    )
+    return ((pwmValue - 1000) / (2000 - 1000)) * 100
   }
 
   return (
@@ -88,7 +49,7 @@ const JoystickDisplay: React.FC = () => {
               <JoystickSquare x={joystickData.roll} y={joystickData.pitch} />
             </div>
           </div>
-          {renderPWMTable()}
+          {joystickData && PWMTable(joystickData)}
         </div>
       )}
     </div>
---
+++ src/renderer/src/components/PWMTable.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { JoystickOutputInterface } from '../../../models/JoystickOutput'
+
+const PWMTable: React.FC<JoystickOutputInterface> = (props) => {
+    // Progress calculation function
+    const calculateProgress = (pwmValue: number) => {
+        return ((pwmValue - 1000) / (2000 - 1000)) * 100
+    }
+    
+    const renderPWMTable = () => {
+        const excludedChannels = ['yaw', 'throttle', 'roll', 'pitch']
+        const filteredData = Object.entries(props).filter(([channel]) => !excludedChannels.includes(channel))
+    
+        return (
+        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+            <table>
+            <thead>
+                <tr>
+                <th>Type</th>
+                <th>PWM</th>
+                <th>Progress</th>
+                </tr>
+            </thead>
+            <tbody>
+                {filteredData.map(([channel, pwmValue]) => (
+                <tr key={channel}>
+                    <td>{channel}</td>
+                    <td>{pwmValue}</td>
+                    <td>
+                    <div style={{ width: '100%', backgroundColor: '#ddd', height: '12px' }}>
+                        <div
+                        style={{
+                            width: `${calculateProgress(pwmValue)}%`,
+                            backgroundColor: 'green',
+                            height: '100%',
+                        }}
+                        />
+                    </div>
+                    </td>
+                </tr>
+                ))}
+            </tbody>
+            </table>
+        </div>
+        )
+    }
+    
+    return renderPWMTable()
+}
+
+export default PWMTable
